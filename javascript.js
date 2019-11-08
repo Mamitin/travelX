ee9a6e82e37b63259e583c7da378a9fdd0249351
Shelby Reinsimar <shelbyreins@yahoo.com>
2019-11-07 17:43:40 -0800

    Start pseudocode

---
+++ javascript.js
@@ -0,0 +1,11 @@
+//HTML
+    // Search bar with our name at the top
+    // A card with the weather, city and the picture
+    // Be able to click on the card 
+    // After clicking on the card pictures will display
+    
+
+//JS
+    // Use firebase authenication
+    // Use AJAX call for weather and photo API
+    // 
