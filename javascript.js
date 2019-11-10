6a02f4ad22fbd479c0ac07391a28fcc659ff41a6
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-11-09 19:21:03 -0800

    confirm weather API works and dinamically generate weather cards

--- javascript.js
+++ javascript.js
@@ -16,45 +16,38 @@
 // AJAX call to photo API/ AJAX call for activities API
 // display images with photo display library
 
-var city = "";
-
 $("#add-city").on("click", function (event) {
     event.preventDefault();
     if ($("#city")[0].reportValidity()) {
-        city = $("#city").val().trim();
+        var city = $("#city").val().trim();
 
-
-        // queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=ad0f64f9a42308c19b2ba725fdc6802c"
-        // $.ajax({
-        //     url: queryUrl,
-        //     method: "GET",
-        // }).then(function (response) {
-        //     for (var i = 0; i < response.list.length; i++){
-        //     var iconCode = response.list[i].weather[0].icon;
-        //     console.log(iconCode);
-        //     // var iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-        //     // var weatherLogo = $("<img>").attr("src", iconURL);
-        //     // $(".icon").append(weatherLogo);
-
-        //     }
-        // });
+        createWeatherCard(city);
     }
-
 })
 
+createWeatherCard("San Francisco");
+createWeatherCard("Paris");
+createWeatherCard("New York");
 
-queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=san+francisco&APPID=ad0f64f9a42308c19b2ba725fdc6802c"
-$.ajax({
-    url: queryUrl,
-    method: "GET",
-}).then(function (response) {
-    for (var i = 0; i < response.list.length; i= i+40){
-    var iconCode = response.list[i].weather[0].icon;
-    console.log(iconCode);
-    var iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-    var weatherLogo = $("<img>").attr("src", iconURL);
-    $(".icon").append(weatherLogo);
-
-    }
-});
-
+function createWeatherCard(city) {
+    queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=ad0f64f9a42308c19b2ba725fdc6802c"
+    $.ajax({
+        url: queryUrl,
+        method: "GET",
+    }).then(function (response) {
+        var iconCode = response.weather[0].icon;
+        var weatherMain = response.weather[0].main;
+        var weatherDescription = response.weather[0].description;
+        var temp = Math.round(response.main.temp);
+        var iconURL = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+        var weatherCard = '<div class="card" style="width: 18rem;" class="weather-card">' +
+            '<div class="card-body">' +
+            '<div class="icon"><img src="' + iconURL + '"></div>' +
+            '<div class="degrees">' + temp + " &deg;F" + '</div>' +
+            '<h5 class="card-title" class="city-name">' + city + '</h5>' +
+            '<div>' + weatherMain + ": " + weatherDescription + '</div>' +
+            '</div>' +
+            '</div>';
+        $("#top-container").append(weatherCard);
+    });
+}
